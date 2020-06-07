1a7494af7d2dd62a0174b3776c8494ae31428cc5
Martin Peck <martinjohnpeck@gmail.com>
2020-06-07 13:45:53 +0000

    binary tree maze implementation

---
+++ binary_tree.rb
@@ -0,0 +1,18 @@
+require 'grid'
+
+class BinaryTree
+  def self.on(grid)
+    grid.each_cell do |cell|
+      neighbours = []
+      neighbours << cell.north if cell.north
+      neighbours << cell.east if cell.east
+
+      index = rand(neighbours.length)
+      neighbour = neighbours[index]
+
+      cell.link(neighbour) if neighbour
+    end
+
+    grid
+  end
+end
---
+++ binary_tree_demo.rb
@@ -0,0 +1,7 @@
+require 'grid'
+require 'binary_tree'
+
+my_grid = Grid.new(10, 10)
+BinaryTree.on(my_grid)
+
+puts my_grid
