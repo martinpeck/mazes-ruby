a75b0e5772a7e87d52e69ead7a49f3b972181c57
Martin Peck <martinjohnpeck@gmail.com>
2020-06-07 16:25:53 +0000

    add sidewinder

---
+++ sidewinder_demo.rb
@@ -0,0 +1,10 @@
+require_relative 'grid'
+require_relative 'sidewinder'
+
+my_grid = Grid.new(30, 30)
+Sidewinder.on(my_grid)
+
+img = my_grid.to_png cell_size:20, wall_size:2
+img.save "output/sidewinder.png"
+
+File.write("output/sidewinder.txt", my_grid)
