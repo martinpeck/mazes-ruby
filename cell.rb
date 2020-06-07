c7d03e7f913bec2e31ae816aa46b12190c6dbc2a
Martin Peck <martinjohnpeck@gmail.com>
2020-06-07 13:45:38 +0000

    cell and grid basics

---
+++ cell.rb
@@ -0,0 +1,39 @@
+class Cell
+  attr_reader :row, :column
+  attr_accessor :north, :south, :east, :west
+
+  def initialize(row, column)
+    @row = row
+    @column = column
+    @links = {}
+  end
+
+  def link(cell, bidi = true)
+    @links[cell] = true
+    cell.link(self, false) if bidi
+    self
+  end
+
+  def unlink(cell, bidi = true)
+    @links.delete(cell)
+    cell.unlink(self, false) if bidi
+    self
+  end
+
+  def links
+    @links.keys
+  end
+
+  def linked?(cell)
+    @links.key?(cell)
+  end
+
+  def neighbours
+    list = []
+    list << north if north
+    list << south if south
+    list << east if east
+    list << west if west
+    list
+  end
+end
