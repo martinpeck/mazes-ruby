1438227a83023336d3ed367d5e5a2615c7202171
Martin Peck <martinjohnpeck@gmail.com>
2020-06-07 21:33:22 +0000

    output distance grids to file

--- dikstra.rb
+++ dikstra.rb
@@ -8,9 +8,13 @@ start_cell = grid[0, 0]
 distances_from_start = start_cell.distances
 
 grid.distances = distances_from_start
-puts grid
 
-puts "shortest path"
+puts grid
+File.write("output/binary_tree_with_dikstra.txt", grid)
+
 breadcrumbs = distances_from_start.path_to(grid[grid.rows - 1, 0])
 grid.distances = breadcrumbs
+
+puts "shortest path..."
+File.write("output/binary_tree_with_dikstra_shortest_path.txt", grid)
 puts grid
